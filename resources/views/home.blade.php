5b138a73aa0197df0b7513bcffb149a7a2c217ae
Yurii Svystun <svystun@gmail.com>
2017-07-24 01:13:38 +0300

    Added hidden meta data and pre-loading icon.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,12 +40,19 @@
                 <h1>Search results for &laquo;{{ ucfirst($user->username) }}&raquo;</h1>
             </div>
             @foreach($items as $item)
-            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6">
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6" id="{{ $item->code }}thumb">
 
-                <div class="thumb">
-                    <img src="{{ $item->images->low_resolution->url }}" alt="" title="">
+                <div class="thumb" id="{{ $item->code }}">
+                    <img src="{{ $item->images->low_resolution->url }}" class="image-url" alt="" title="">
+                    <div style="display: none">
+                        <span class="thumb-id">{{ $item->id }}</span>
+                        <span class="caption-text">{{ $item->caption->text }}</span>
+                        <span class="user-name">{{ $item->user->username }}</span>
+                        <span class="time">{{ $item->created_time }}</span>
+                        <span class="type">{{ $item->type }}</span>
+                    </div>
                     <div class="caption">
-                        <h5>
+                        <h5 class="location-name">
                             @if(isset($item->location->name))
                                 {{ $item->location->name }}
                             @else
@@ -53,9 +60,10 @@
                             @endif
                         </h5>
                         <p>
-                            <button type="button" class="label label-danger" @click="reverseMessage(`{{ $item->location->name }}`)">
+                            <button v-if="!loading" type="button" class="label label-danger" @click="storePost(`{{ $item->code }}`)">
                                 SAVE
                             </button>
+                            <span v-if="loading"><img src="{{ asset('images/loading.gif') }}"></span>
                         </p>
                     </div>
                 </div>
