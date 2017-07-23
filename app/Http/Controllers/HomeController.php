8ae0f9707361eb17bc139ba6b75fb97575008fff
Yurii Svystun <svystun@gmail.com>
2017-07-23 16:32:28 +0300

    Added search of user data by nickname.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,28 +1,44 @@
-<?php
+<?php namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
-
+use App\Repositories\InstagramRepository;
 use Illuminate\Http\Request;
 
+/**
+ * Class HomeController
+ * @package App\Http\Controllers
+ */
 class HomeController extends Controller
 {
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //
     }
 
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param InstagramRepository $instagram
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function index()
+    public function index(Request $request, InstagramRepository $instagram)
     {
-        return view('home');
+        $items = []; $errors = []; $user = NULL;
+        if ($request->has('search')) {
+            try {
+                $user = $instagram->getUser($request->get('search'));
+                if ($user->is_private) {
+                    throw new \Exception('It`s private account! Content is not accessible');
+                }
+                $items = $instagram->getItems($request->get('search'));
+            } catch (\Exception $e) {
+                $errors[] = $e->getMessage();
+            }
+        }
+
+        return view('home', compact('user', 'items'))->withErrors($errors);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,66 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <div class="panel-heading">Search bar</div>
                 <div class="panel-body">
-                    You are logged in!
+                    <form method="GET" action="{{ route('home') }}">
+                        <div class="form-group{{ $errors->has('search') ? ' has-error' : '' }}">
+                            <div class="center-block" style="width: 500px">
+                                <input type="text" class="form-control" name="search"
+                                       value="{{ old('search', request()->get('search')) }}"
+                                       placeholder="Search user..." required autofocus>
+
+                                @if ($errors->has('search'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('search') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+
+    @if(count($items))
+        <div class="row">
+            <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <h1>Search results for &laquo;{{ ucfirst($user->username) }}&raquo;</h1>
+            </div>
+            @foreach($items as $item)
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6">
+
+                <div class="thumb">
+                    <img src="{{ $item->images->low_resolution->url }}" alt="" title="">
+                    <div class="caption">
+                        <h5>
+                            @if(isset($item->location->name))
+                                {{ $item->location->name }}
+                            @else
+                                No location
+                            @endif
+                        </h5>
+                        <p>
+                            <button type="button" class="label label-danger" @click="reverseMessage(`{{ $item->location->name }}`)">
+                                SAVE
+                            </button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    @endif
+
 </div>
 @endsection
