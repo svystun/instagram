9ee68e0f0e7120ddd6aa943f83f66b36677342e5
Yurii Svystun <svystun@gmail.com>
2018-05-15 03:34:42 +0300

    Added parse test.

---
+++ tests/Browser/ParseTest.php
@@ -0,0 +1,26 @@
+<?php namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ParseTest extends DuskTestCase
+{
+    /**
+     * @throws \Throwable
+     */
+    public function testSearch()
+    {
+        $faker = \Faker\Factory::create();
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::first())
+                ->visit('/')
+                ->assertSee('Search bar')
+                ->type('search', 'paris')
+                ->press('Search')
+                ->assertSee('Paris');
+        });
+    }
+}
