2b055b3ec605d7e1c4df8ac8791fc4b1d922bdf3
Yurii Svystun <svystun@gmail.com>
2018-05-16 02:56:53 +0300

    Fixed some bugs in controller.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,11 +1,12 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use InstagramScraper\Instagram;
-use InstagramScraper\Exception\{InstagramNotFoundException, InstagramException};
-use Illuminate\Support\Facades\Auth;
 use App\Posts;
+use Illuminate\Http\Request;
+use InstagramScraper\Instagram;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use InstagramScraper\Exception\{InstagramNotFoundException, InstagramException};
+use Exception;
 
 /**
  * Class HomeController
@@ -43,41 +44,57 @@ class HomeController extends Controller
      */
     public function store(Request $request)
     {
-//      TODO: Add validation
-//        Validator::make($request->all(), [
-//            'title' => 'required|unique:posts|max:255',
-//            'body' => 'required',
-//        ])->validate();
+        $status = ['status' => 'success', 'message' => 'No errors'];
 
-        $response = $this->isExists($request);
+        try {
+            $this->isValid($request);
+            $this->isExists($request->input('insta_id'));
 
-        if ($response['status'] == 'error') {
-            return response($response);
+            $request->merge(['user_id' => Auth::user()->id]);
+            Posts::create($request->all());
+
+        } catch (Exception $e) {
+            $status['status'] = 'error';
+            $status['message'] = $e->getMessage();
         }
 
-        $request->merge(['user_id' => Auth::user()->id]);
-        Posts::create($request->all());
+        return response()->json($status);
+    }
 
-        return response($response);
+    /**
+     * Validate request
+     *
+     * @param Request $request
+     * @throws Exception
+     */
+    protected function isValid(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'insta_id'      => 'required|numeric|min:1',
+            'insta_url'     => 'required|active_url|max:500',
+            'insta_caption' => 'required|string|max:500',
+            'insta_user'    => 'required|string|max:255',
+            'insta_name'    => 'required|string|max:255',
+            'insta_time'    => 'required|numeric|min:1000000000',
+            'insta_type'    => 'required|string|max:100'
+        ]);
+
+        if ($validator->fails()) {
+            throw new Exception($validator->errors()->first());
+        }
     }
 
     /**
      * Check if content exists
      *
-     * @param Request $request
-     * @return array
+     * @param string $insta_id
+     * @throws Exception
      */
-    public function isExists(Request $request)
+    protected function isExists(string $insta_id)
     {
-        $status = ['status' => 'success', 'message' => 'No errors'];
-
-        if (Posts::where('insta_id', $request->input('insta_id'))->first()) {
-            $status['status'] = 'error';
-            $status['message'] = 'Already exists!';
-            return $status;
+        if (Posts::where('insta_id', $insta_id)->first()) {
+            throw new Exception('Already exists!');
         }
-
-        return $status;
     }
 
     /**
